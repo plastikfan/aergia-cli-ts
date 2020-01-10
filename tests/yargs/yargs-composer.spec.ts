dd529c557aab83c2683b1af06f0f5cd91e466565
plastikfan <plastikfan@zoho.com>
2020-01-10 11:40:58 +0000

    chore(yargs-composer): invoke index (coverage)

--- tests/yargs/yargs-composer.spec.ts
+++ tests/yargs/yargs-composer.spec.ts
@@ -3,12 +3,17 @@ import { expect, use } from 'chai';
 import dirtyChai = require('dirty-chai');
 use(dirtyChai);
 import * as composer from '../../lib/yargs/yargs-composer';
+import { index } from '../../lib/index';
 
 describe('yargs-composer', () => {
-  context('given:', () => {
-    it('should:', () => {
+  context('given: ...', () => {
+    it('should: dummy', () => {
       composer.dummy();
       expect(1).to.equal(1);
+    });
+
+    it('should: index', () => {
+      index();
     });
   });
 });
